f039c2efbdfe9f1ee790615fbe0ab49a15ac31b6
Bogdan Tsegichko <sirbony@gmail.com>
2023-09-12 22:13:06 +0300

    created new test api

--- src/api/tests/test_api.py
+++ src/api/tests/test_api.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework import status
 from rest_framework.status import (HTTP_200_OK, HTTP_201_CREATED,
                                    HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED)
 from rest_framework.test import APIClient
@@ -24,6 +24,7 @@ class CarAPITestCase(TestCase):
 
     def test_car_create_view(self):
         self.client.force_authenticate(user=self.user)
+        image = SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg")
         category = Category.objects.create(name="One")
         data = {
             'category': category.id,
@@ -36,10 +37,15 @@ class CarAPITestCase(TestCase):
             'mileage': 15000,
             'color': 'Black',
             'price': 28000,
-            'image': 'https://cdn2.riastatic.com/photosnew/auto/photo/bmw_4-series__511308392f.webp',
         }
+        files = {'image': ('test_image.jpg', image)}
+        response = self.client.post(
+            reverse("api:car-create"),
+            data,
+            format='multipart',
+            files=files,
+        )
 
-        response = self.client.post(reverse("api:car-create"), data, format='json')
         self.assertEqual(response.status_code, HTTP_201_CREATED)
 
     def test_car_delete_view(self):
@@ -47,3 +53,34 @@ class CarAPITestCase(TestCase):
         self.car = create_sample_car()
         response = self.client.delete(reverse("api:car-delete", kwargs={"pk": self.car.id}))
         self.assertEqual(response.status_code, HTTP_204_NO_CONTENT)
+
+    def test_car_update_view(self):
+        self.client.force_authenticate(user=self.user)
+        car = create_sample_car()
+        new_data = {
+            'name': 'Updated Camry',
+            'year': 2023,
+            'mileage': 20000,
+        }
+        response = self.client.patch(
+            reverse("api:car-update", kwargs={"pk": car.id}),
+            new_data,
+            format='json',
+        )
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        updated_car = Car.objects.get(pk=car.id)
+        self.assertEqual(updated_car.name, new_data['name'])
+        self.assertEqual(updated_car.year, new_data['year'])
+        self.assertEqual(updated_car.mileage, new_data['mileage'])
+
+    def test_car_retrieve_view(self):
+        self.client.force_authenticate(user=self.user)
+        car = create_sample_car()
+        response = self.client.get(reverse("api:car-detail", kwargs={"pk": car.id}))
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(response.data['name'], car.name)
+        self.assertEqual(response.data['year'], car.year)
+
+    def test_unauthorized_user_view(self):
+        response = self.client.get('/api/cars/create/')
+        self.assertEqual(response.status_code, HTTP_401_UNAUTHORIZED)
--- src/api/views.py
+++ src/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
-                                     ListAPIView, RetrieveUpdateAPIView)
+                                     ListAPIView, RetrieveAPIView,
+                                     RetrieveUpdateAPIView)
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -22,16 +23,24 @@ class CarListView(ListAPIView):
 
 
 class CarCreateView(CreateAPIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsSuperUser]
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
 
 class CarUpdateView(RetrieveUpdateAPIView):
+    permission_classes = [IsSuperUser]
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
 
 class CarDeleteView(DestroyAPIView):
+    permission_classes = [IsSuperUser]
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+
+class CarDetailView(RetrieveAPIView):
+    permission_classes = [AllowAny]
     queryset = Car.objects.all()
     serializer_class = CarSerializer
