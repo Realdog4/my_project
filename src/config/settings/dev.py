35d4ded1cadc59f1575795c306c27dd29208fdbc
Bogdan Tsegichko <sirbony@gmail.com>
2023-09-18 01:16:15 +0300

    settings ci-cd

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -9,15 +9,15 @@ ALLOWED_HOSTS = []
 SECRET_KEY = "django-secret-key"
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": os.environ.get("POSTGRES_DB"),
+    #     "USER": os.environ.get("POSTGRES_USER"),
+    #     "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+    #     "HOST": os.environ.get("POSTGRES_HOST"),
+    #     "PORT": os.environ.get("POSTGRES_PORT")
+    # },
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT")
-    },
-    "sqllite": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",  # NOQA
     }
